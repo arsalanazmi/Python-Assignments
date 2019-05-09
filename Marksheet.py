bde7a6fe2244f3a1bf1d2c5b0429b6755c220269
arsalanazmi <arsalan.azmi92@gmail.com>
2019-05-09 17:28:50 +0500

    Marks Obtained added

--- Marksheet.py
+++ Marksheet.py
@@ -1,14 +1,16 @@
+print("\n")
 Name = input("Student's Name = ")
 Roll_No = int(input("Student's Roll No. = "))
+print("\n")
 
 English = int(input("Marks of English = "))
-Urdu = int(input("Marks of Urdu = "))
+Sindhi = int(input("Marks of Sindhi = "))
 Chemistry = int(input("Marks of Chemistry = "))
 Biology = int(input("Marks of Biology = "))
 Pak_Studies = int(input("Marks of Pak-Studies = "))
 
 Total_Marks = 500
-Marks_Obtained = English + Urdu + Chemistry + Biology + Pak_Studies
+Marks_Obtained = English + Sindhi + Chemistry + Biology + Pak_Studies
 Percentage = round(((Marks_Obtained/Total_Marks) * 100),2)
 
 print("\n")
@@ -17,11 +19,12 @@ print("\n")
 print("SUBJECTS          OBTAINED TOTAL")
 print("_________________________________")
 print("English         :    "  + str(English)  + "     " + str(100) )
-print("Urdu            :    "  + str(Urdu)  + "     " +  str(100) )
+print("Sindhi          :    "  + str(Sindhi)  + "     " +  str(100) )
 print("Chemistry       :    "  + str(Chemistry) + "     " +   str(100) )
 print("Biology         :    "  + str(Biology)  + "     " +  str(100)  )
 print("Pak-Studies     :    "  + str(Pak_Studies) + "     " +  str(100) )
 print("_________________________________")
+print("Marks Obtained  :  " +  str(Marks_Obtained) + " out of " + str(500))
 print("Percentage      :  " +  str(Percentage) + " %")
 
 
