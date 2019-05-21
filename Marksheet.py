463660a06bee05db9365ffda5222ac9ca30787fe
arsalanazmi <arsalan.azmi92@gmail.com>
2019-05-21 05:32:48 +0500

    Assignment#2

--- Marksheet.py
+++ Marksheet.py
@@ -1,6 +1,7 @@
 print("\n")
 Name = input("Student's Name = ")
 Roll_No = int(input("Student's Roll No. = "))
+School_Name = input("School Name = ")
 print("\n")
 
 English = int(input("Marks of English = "))
@@ -14,29 +15,29 @@ Marks_Obtained = English + Sindhi + Chemistry + Biology + Pak_Studies
 Percentage = round(((Marks_Obtained/Total_Marks) * 100),2)
 
 print("\n")
-print("------------MARKSHEET------------")
+print("-------------MARKSHEET-------------")
 print("\n")
-print("SUBJECTS          OBTAINED TOTAL")
-print("_________________________________")
-print("English         :    "  + str(English)  + "     " + str(100) )
-print("Sindhi          :    "  + str(Sindhi)  + "     " +  str(100) )
-print("Chemistry       :    "  + str(Chemistry) + "     " +   str(100) )
-print("Biology         :    "  + str(Biology)  + "     " +  str(100)  )
-print("Pak-Studies     :    "  + str(Pak_Studies) + "     " +  str(100) )
-print("_________________________________")
-print("Marks Obtained  :  " +  str(Marks_Obtained) + " out of " + str(Total_Marks))
-print("Percentage      :  " +  str(Percentage) + " %")
+print("SUBJECTS \t   OBTAINED   MAX.")
+print("___________________________________")
+print("English\t\t :    " + str(English) + "      " + str(100) )
+print("Sindhi\t\t :    "  + str(Sindhi)  + "      " +  str(100) )
+print("Chemistry\t :    "  + str(Chemistry) + "      " +   str(100) )
+print("Biology\t\t :    "  + str(Biology)  + "      " +  str(100)  )
+print("Pak-Studies\t :    "  + str(Pak_Studies) + "      " +  str(100) )
+print("___________________________________")
+print("TOTAL\t\t :    " +  str(Marks_Obtained) + "     " + str(Total_Marks))
+print("PERCENTAGE\t :    " +  str(Percentage) + " %")
 
 
 if (Percentage >= 80) :
-    print("Grade           :  A-ONE")
+    print("GRADE\t\t :    A-ONE")
 elif (Percentage >= 70 and Percentage < 80) :
-    print("Grade           :  A")
+    print("GRADE\t\t :    A")
 elif (Percentage >= 60 and Percentage < 70) :
-    print("Grade           :  B")
+    print("GRADE\t\t :    B")
 elif (Percentage >= 50 and Percentage < 60) :
-    print("Grade           :  C")
+    print("GRADE\t\t :    C")
 elif (Percentage >= 40 and Percentage < 50) :
-    print("Grade           :  D")
+    print("GRADE\t\t :    D")
 else :
-    print("Grade           :  Fail")
+    print("GRADE\t\t :    Fail")
