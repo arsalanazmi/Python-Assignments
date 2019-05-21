a8cab9ee51216048bbf8540ca0f0bf6813cb0526
arsalanazmi <arsalan.azmi92@gmail.com>
2019-05-21 05:37:24 +0500

    Assignment#4

---
+++ Triangle.py
@@ -0,0 +1,138 @@
+print("\nTYPES OF TRIANGLE \n")
+
+for rows in range (0,10):
+    for cols in range (0,rows+1):
+        print("*", end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+
+# TYPES OF TRIANGLE
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+# * * * * * * *
+# * * * * * * * *
+# * * * * * * * * *
+# * * * * * * * * * *
+# ___________________________
+
+for rows in reversed(range (0,10)):
+    for cols in range (0,rows+1):
+        print("*", end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+# * * * * * * * * * *
+# * * * * * * * * *
+# * * * * * * * *
+# * * * * * * *
+# * * * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# ___________________________
+
+n=10
+k=2*n-2 #No. of spaces
+for rows in range (0,n):
+    for cols in range (0,k):
+        print(end=" ")
+    k=k-2
+    for j in range(0,rows+1):
+        print("*",end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+#                   *
+#                 * *
+#               * * *
+#             * * * *
+#           * * * * *
+#         * * * * * *
+#       * * * * * * *
+#     * * * * * * * *
+#   * * * * * * * * *
+# * * * * * * * * * *
+# ___________________________
+
+n=10
+k=0 #No. of spaces
+for rows in reversed(range (0,n)):
+    for cols in range (0,k):
+        print(end=" ")
+    k=k+2
+    for j in range(0,rows+1):
+        print("*",end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+# * * * * * * * * * *
+#   * * * * * * * * *
+#     * * * * * * * *
+#       * * * * * * *
+#         * * * * * *
+#           * * * * *
+#             * * * *
+#               * * *
+#                 * *
+#                   *
+# ___________________________
+
+n=10
+k=n-1 #No. of spaces
+for rows in range (0,n):
+    for cols in range (0,k):
+        print(end=" ")
+    k=k-1
+    for j in range(0,rows+1):
+        print("*",end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+#          *
+#         * *
+#        * * *
+#       * * * *
+#      * * * * *
+#     * * * * * *
+#    * * * * * * *
+#   * * * * * * * *
+#  * * * * * * * * *
+# * * * * * * * * * *
+# ___________________________
+
+n=10
+k=0  #No. of spaces
+for rows in reversed(range (0,n)):
+    for cols in range (0,k):
+        print(end=" ")
+    k=k+1
+    for j in range(0,rows+1):
+        print("*",end=" ")
+    print("\r")
+print("___________________________\n")
+
+# OUTPUT:
+# * * * * * * * * * *
+#  * * * * * * * * *
+#   * * * * * * * *
+#    * * * * * * *
+#     * * * * * *
+#      * * * * *
+#       * * * *
+#        * * *
+#         * *
+#          *
+# ___________________________
