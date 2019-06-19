74b84d35890c4a68657964c0f19ba75061051bb9
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-19 20:49:56 +0500

    7. Diff. b/w given no. & 17, diff. can not be -ve

---
+++ Number_Difference.py
@@ -0,0 +1,9 @@
+# TASK # 07
+# DETERMINE THE DIFF. B/W GIVEN NO. & 17 & THE DIFF. CAN NOT BE IN NEGATIVE
+
+Number = int(input("Enter any number: "))
+Given_Number = 17
+
+diff = abs(Number - Given_Number)
+print("Given Number =",Given_Number)
+print("Difference = ",diff)
