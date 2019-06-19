4c664f0124efe3f281ab72264c10ac6f259ae4a4
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-20 00:43:16 +0500

    14. Solve the equation

---
+++ Equation.py
@@ -0,0 +1,9 @@
+# TASK # 14
+# Solve THE FOLLOWING EQUATION
+
+x = int(input("Enter Value of x: "))
+y = int(input("Enter value of y: "))
+
+result = (x + y) * (x + y)
+print("Equation = (x + y) * (x + y)")
+print("Result:",result)
