5bc37d1451b4362394b93e56f9a4d352951c4719
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-20 03:17:20 +0500

    39. Multiplication table (2-10)

--- Tables.py
+++ Tables.py
@@ -1,5 +1,7 @@
-# Multiplication Table from (1-10)
-for i in range (1,11) :
+# TASK # 39
+# Multiplication Table from (2-10)
+
+for i in range (2,11) :
     for j in range (1,11) :
         print(i, "*", j, "=", i*j)
     print("\n**************************\n")
