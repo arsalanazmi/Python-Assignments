518083742960cfa831f213883654f8e281af17ab
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-19 21:02:02 +0500

    10. Even/Odd

---
+++ Even_Odd.py
@@ -0,0 +1,9 @@
+# TASK # 10
+# TO CHECK WHETHER THE GIVEN NO. IS EVEN OR ODD!
+
+number = int(input("Enter any number: "))
+
+if (number % 2) == 0 :
+    print("Given number", number,"is Even")
+else:
+    print("Given number", number,"is Odd")
