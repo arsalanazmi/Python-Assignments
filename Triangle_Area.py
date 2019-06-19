05bece957122822ae1b205f9f0956b0ce8c80ec5
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-19 21:06:10 +0500

    12. Area of Triangle

---
+++ Triangle_Area.py
@@ -0,0 +1,8 @@
+# TASK # 12
+# COMPUTE THE AREA OF TRIANGLE BY TAKING BASE & HEIGHT AS USER INPUT
+
+base = int(input("Enter Base of Triangle: "))
+height = int(input("Enter Height of Triangle: "))
+
+area = (1/2) * base * height
+print("Area Of Triangle = 1/2 b.h = ",area)
