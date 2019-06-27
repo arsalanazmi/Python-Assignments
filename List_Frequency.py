33363788b557c7a987a2f17407fedf60787f804e
arsalanazmi <arsalan.azmi92@gmail.com>
2019-06-28 02:56:57 +0500

    Frequency of a list

---
+++ List_Frequency.py
@@ -0,0 +1,28 @@
+# Determine Frequency of a List Using Function
+def freq(list):
+    freq_table = {}
+    for i in list:
+        if i in freq_table:
+            freq_table[i] += 1
+        else:
+            freq_table[i] = 1
+    return freq_table
+
+print(freq(["Apple","Apple",'Grapes',"banana","Grapes"]))
+
+
+# Determine Frequency of a List from User Input List Items
+List = []
+n = int(input("Enter no. of Fruits to be added in the list: "))
+for i in range(0,n):
+    fruits = input("Enter Fruit: ")
+    List.append(fruits)
+print("List of Fruits:",List)
+
+freq = {}
+for i in List:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1    
+print("Frequency of Fruits:",freq)
